f7675ca72e0b814d34c08eb5f47fe59112bda602
Petro Verheles <petro.verheles@comergence.biz>
2019-03-24 14:36:27 +0200

    Added comments WordsExtractorService

--- src/main/java/com/virtuace/groupchat/controller/ChatController.java
+++ src/main/java/com/virtuace/groupchat/controller/ChatController.java
@@ -12,8 +12,6 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
 @Controller
 public class ChatController {
 
--- src/main/java/com/virtuace/groupchat/service/WordsExtractorService.java
+++ src/main/java/com/virtuace/groupchat/service/WordsExtractorService.java
@@ -12,12 +12,20 @@ import java.util.regex.Pattern;
 public class WordsExtractorService {
     private static final Pattern PATTERN = Pattern.compile("(?<!\\pL|\\pN)\\pL+(?!\\pL|\\pN)");
 
-    public Iterable<String> extract(String s) {
-        if (s == null) {
+    /**
+     *
+     * Extracts all the words in the given string, stripping numbers, punctuation and special chars
+     * The extracted words are transformed to lower case
+     *
+     * @param sentence incoming sentence
+     * @return the extracted list of words
+     */
+    public Iterable<String> extract(String sentence) {
+        if (sentence == null) {
             return Collections.emptyList();
         }
 
-        Matcher matcher = PATTERN.matcher(s);
+        Matcher matcher = PATTERN.matcher(sentence);
 
         List<String> words = new ArrayList<>();
         while (matcher.find()) {
